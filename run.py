d06f11efecdfc96bd9a3a83221fb0f3eb3b389bf
Dima <dima.yerosh@gmail.com>
2021-03-27 00:34:42 +0100

    modified env port variable to run in prod mode; modified dockerfile for local testing

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
@@ -18,4 +19,5 @@ def createApp() -> 'app':
 
 
 if __name__ == '__main__':
-    createApp().run(host='0.0.0.0', debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    createApp().run(host='0.0.0.0', port=port, debug=False)
